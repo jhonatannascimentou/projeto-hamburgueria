27e1ea303908b2be5f4d70ad54662a36553c5542
Jhonatan <j.lesniovies.ext@sascar.com.br>
2025-09-30 21:34:42 -0300

    Teste

--- utils/pedidoUtils.js
+++ utils/pedidoUtils.js
@@ -3,6 +3,7 @@
 const fetch = require('node-fetch');
 const { nomePedido } = require('../public/app.js');
 
+// Função para criar um card no Trello
 const criarCardTrello = async () => {
     try {
         const response = await fetch(`https://api.trello.com/1/cards?idList=68dc6c0091ad86a76589c71f&key=8055bc05a6e0e98eb3ba6de21769af93&token=ATTAe8bd7d8962f56eda910ebb0f08540d0784839737c0a91bcd505d1440bc8e2ffcD79CFE46&name=${nomePedido}`, {
